83c23f02698c061d1f66223d050381a8c10711c5
Ali Hassaine <ali.hs@outlook.fr>
2022-11-28 17:43:51 +0000

    Setup colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        soft: {
+          orange: 'hsl(35, 77%, 62%)',
+          red: 'hsl(5, 85%, 63%)',
+        },
+        neutral: {
+          offwhite: 'hsl(36, 100%, 99%)',
+          grayblue: 'hsl(233, 8%, 79%)',
+          darkgrayblue: 'hsl(236, 13%, 42%)',
+          darkblue: 'hsl(240, 100%, 5%)',
+        },
+      },
+    },
   },
   plugins: [],
 };
